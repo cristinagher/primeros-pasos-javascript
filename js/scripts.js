6a3eef00b4162f807db701532fec3706a8dbea34
Patricia Serrano <cristinagamezhernandez@gmail.com>
2024-04-04 18:56:10 +0200

    js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function sayHello (name, age) {
+    const nextAge= age + 1;
+    console.log ('Hola ' + name, ', el año que viene tendrás ' + nextAge + ' años.');
+}
+
+sayHello('Keko', 21);
+sayHello('Laura', 87);
+sayHello('Ana', 58);
+sayHello('Pedrito', 9);
